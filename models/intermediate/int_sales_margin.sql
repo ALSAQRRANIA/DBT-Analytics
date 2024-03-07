84a5c184dbe7b62da5b30bc92e4c3052ced42da8
alsaqrrania@gmail.com <alsaqrrania@gmail.com>
2024-03-07 08:31:15 +0000

    c

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,9 +6,10 @@ SELECT
     quantity, 
     CAST(purchSE_PRICE AS FLOAT64), 
     ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    s.revenue - ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS margin
     FROM {{ref("stg_raw__sales")}} s
-    LEFT JOIN {{ref("stg_raw__product")}} p 
-     USING (products_id)
+    LEFT JOIN {{ref("stg_raw__product")}} p USING (products_id)
+
+
     
     
